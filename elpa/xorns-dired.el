149ddd5f4ceb6af955c78656f682557b21412c21
Medardo Rodriguez <med@merchise.org>
2018-01-19 14:07:53 -0500

    Use new utility functions in 'xorns-dired'.

--- elpa/xorns-dired.el
+++ elpa/xorns-dired.el
@@ -53,15 +53,16 @@
   (require 'ns nil 'noerror))
 
 
-(setq
-  dired-dwim-target t
+(xorns-set-values
+  '(dired-dwim-target t)
   ;; Next is because `ls' used in Mac Os X is a BSD version and it doesn't
   ;; have any extended option (those starting with `--'), particularly
   ;; Merchise preferred `group-directories-first'.
-  dired-listing-switches
-    (if (-mac-os)
-      "-lah"
-      "-la --group-directories-first -h")
+  '(dired-listing-switches
+     (if (-mac-os)
+       "-lah"
+       ;; else
+       "-la --group-directories-first -h"))
   )
 
 
