a8d229bfa64eb2711257eaf32e363620d592c5cb
Medardo Antonio Rodriguez <med@merchise.org>
2019-06-18 09:56:43 -0400

    Start migrating lisp package to use 'make'.

--- lisp/xorns-pkg.el
+++
@@ -1,5 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package
-  "xorns" "20181018.1415"
-  "Merchise extensions for Emacs"
-  '((emacs "26.1") (dash "20180726.1213") (f "20180106.922") (s "20180406.808") (use-package "20180715.1801")))
