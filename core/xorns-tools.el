3fd0ac1d91e9de1f56ee7b0623a12249d245317a
Medardo Antonio Rodriguez <med@merchise.org>
2019-07-29 09:27:18 -0400

    Add function to determine if current buffer is remote

--- core/xorns-tools.el
+++ core/xorns-tools.el
@@ -13,6 +13,9 @@
 
 ;;; Code:
 
+
+;; Debug
+
 (defmacro >>=on-debug-message (format-string &rest args)
   "Display a message only when `init-file-debug' is active.
 Use the same parameters as `message' standard function: FORMAT-STRING and
@@ -20,6 +23,9 @@ ARGS."
   `(if init-file-debug
     (message (concat ">>= " ,format-string) ,@args)))
 
+
+
+;; String - symbol conversion
 
 (defsubst >>=intern (string)
   "Return STRING\'s canonical symbol (safe if it is already a symbol)."
@@ -30,6 +36,9 @@ ARGS."
   "Return SYMBOL\'s name, a string (safe if it is already a string)."
   (if (stringp symbol) symbol (symbol-name symbol)))
 
+
+
+;; property list extension
 
 (defun plist-exclude (plist &rest props)
   "Return a copy of PLIST with all PROPS excluded.
@@ -43,6 +52,9 @@ PLIST is a property-list of the form (PROP1 VALUE1 PROP2 VALUE2 ...)."
 	  (push key res))))
     res))
 
+
+
+;; Files and directories
 
 (defun dir-join (&rest parts)
   "Join PARTS to a single path."
@@ -58,6 +70,26 @@ PLIST is a property-list of the form (PROP1 VALUE1 PROP2 VALUE2 ...)."
 	  (setq res dir))))
     res))
 
+
+
+;; Buffers
+
+(defun >>=current-buffer-remote? ()
+  "Return non-nil if current buffer is remote."
+  (require 'files)
+  (let ((tests
+	  (list
+	    (buffer-file-name)
+	    list-buffers-directory
+	    default-directory))
+	res)
+    (while (and tests (not res))
+      (let ((aux (car tests)))
+	(if (and (stringp aux) (file-remote-p aux))
+	  (setq res aux)
+	  ; else: next item
+	  (setq tests (cdr tests)))))
+    res))
 
 
 (provide 'xorns-tools)
