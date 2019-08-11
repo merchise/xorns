d52d4ee989bda9d51313ff1bcc52352069effae8
Medardo Antonio Rodriguez <med@merchise.org>
2019-08-11 10:19:03 -0400

    Rename 'lisp/xorns-package-archives' -> 'package-archives'

--- lisp/xorns-package-archives.el
+++
@@ -1,16 +0,0 @@
-;;; xorns-package-archives.el --- ELPA repository for packages supported by Xorns
-
-;; Copyright (c) Merchise Autrement [~º/~]
-
-
-;;; Code:
-
-(require 'package)
-
-(setq package-archives
-  '(("melpa" . "https://melpa.org/packages/")
-    ("org" . "https://orgmode.org/elpa/")
-    ("gnu" . "https://elpa.gnu.org/packages/")))
-
-(provide 'xorns-package-archives)
-;;; xorns-package-archives.el ends here
