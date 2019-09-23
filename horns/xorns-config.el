21667cae898f89d4396881214587be169ce89067
Medardo Antonio Rodriguez <med@merchise.org>
2019-09-23 11:44:11 -0400

    Automate user configuration file installation

--- horns/xorns-config.el
+++ horns/xorns-config.el
@@ -22,18 +22,33 @@
 
 
 (with-eval-after-load 'xorns-config
-  (if (not custom-file)
-    (let ((config-file (>>=-config-file-name)))
-      (setq custom-file
-	(if (or (file-exists-p config-file) (>>=-create-new config-file))
-	  config-file
-	  ;; else
-	  (>>=locate-user-emacs-file "custom-${USER}.el" "custom.el")))
-      (when (file-exists-p custom-file)
-	(load custom-file (not init-file-debug))
-	(->? >>=settings/init)))
+  (if custom-file
+    (warn ">>= `custom-file' already assigned with value '%s'." custom-file)
     ;; else
-    (warn ">>= `custom-file' already assigned with value '%s'." custom-file)))
+    (require 'cus-edit)
+    (setq custom-file (>>=-config-file-name))
+    (let ((exists (file-exists-p custom-file))
+	  save)
+      (unless exists
+	(setq exists (>>=-copy-from-template))
+	(let ((old (>>=locate-user-emacs-file
+		     "custom-${USER}.el" "custom.el")))
+	  (when (file-exists-p old)
+	    (message ">>= loading old custom-file '%s' this first time." old)
+	    (load old (not init-file-debug))
+	    (setq save t))))
+      (when exists
+	(load custom-file (not init-file-debug))
+	(->? >>=settings/init))
+      (if save
+	(if exists
+	  (progn
+	    (message ">>= ensure saving imported old variables.")
+	    (custom-save-all))
+	  ;; else
+	  (warn (concat ">>= old variables not saved because new custom-file "
+		  "couldn't be configured from a template; this error must "
+		  "be fixed by hand.")))))))
 
 
 (defun >>=-package-user-dir ()
@@ -61,17 +76,13 @@
       (or xdg "~"))))
 
 
-(defun >>=-create-new (target)
-  "Create new `custom-file' in TARGET from template."
+(defun >>=-copy-from-template ()
+  "Create new `custom-file' from template."
   (let ((template (>>=-template-location)))
-    (if (file-exists-p template)
-      (progn
-	(copy-file template target t)
-	(message ">>= new `custom-file' '%s' has been created." target)
-	template)
-      ;; else
-      (message ">>= `custom-file' template '%s' does not exist." template)
-      nil)))
+    (when (file-exists-p template)
+      (copy-file template custom-file t)
+      (message ">>= new `custom-file' '%s' has been created." custom-file)
+      template)))
 
 
 (provide 'xorns-config)
