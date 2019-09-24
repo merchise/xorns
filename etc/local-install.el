039ad78b527f098f404c3d7f72dbcd4e6531506c
Medardo Antonio Rodriguez <med@merchise.org>
2019-09-24 12:17:04 -0400

    Fix messages in 'local-install' script

--- etc/local-install.el
+++ etc/local-install.el
@@ -12,19 +12,19 @@
     (let ((pkg-desc (cadr (assq pkg package-alist))))
       (if pkg-desc
 	(progn
-	  (message "Deleting old package: %s." pkg)
+	  (message ">>= deleting old package: %s." pkg)
 	  (package-delete pkg-desc 'force 'nosave))
 	;; else
-	(message "Old package '%s' not installed." pkg)))
+	(message ">>= old package '%s' not installed." pkg)))
     ;; Install new version
     (let ((pkg-desc (cadr (assq pkg package-alist)))
 	  (src (expand-file-name "horns" pkg-dir))
 	  (tmp (expand-file-name (symbol-name pkg) temporary-file-directory)))
-      (message "Creating symbolic link: %s -> %s" src tmp)
+      (message ">>= creating symbolic link: %s -> %s" src tmp)
       (make-symbolic-link src tmp 'ok-if-exists)
-      (message "Installing new version of package.")
+      (message ">>= installing new version of package.")
       (package-install-file (file-name-as-directory tmp))
-      (message "Deleting symbolic link: %s" tmp)
+      (message ">>= deleting symbolic link: %s" tmp)
       (delete-file tmp))
     ;; Copy templates to installed package folder
     (let ((elpa-dir (file-name-as-directory package-user-dir))
@@ -34,11 +34,11 @@
 			(file-name-as-directory "horns") "templates"))
 	      (dest (file-name-as-directory
 		       (concat elpa-dir (package-desc-full-name pkg-desc)))))
-	  (message "Copying templates to installed package folder -- %s."
-	    (concat "rsync -auv " source " " dest))
+	  (message ">>= copying templates to installed package folder.")
 	  (shell-command (concat "rsync -auv " source " " dest)))
 	;; else
-	(message "Templates not copied because '%s' is not installed." pkg)))
+	(message ">>= templates not copied, '%s' is not properly installed."
+	  pkg)))
     ))
 
 
