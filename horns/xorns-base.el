19b734e627d1df381d8bbd726fd51945c5a9962c
Medardo Antonio Rodriguez <med@merchise.org>
2019-10-27 16:52:08 -0400

    Generalize initial buffer geometry configuration

--- horns/xorns-base.el
+++ horns/xorns-base.el
@@ -122,22 +122,29 @@ to configure for yourself: see `save-buffer' function for more information.")
 (use-package frame
   :when (display-graphic-p)
   :init
-  (let ((geometry-params
-	  '((internal-border-width . 0)
-	    (fullscreen . maximized)
-	    (fullscreen-restore . maximized)
-	    (undecorated . t))))
+  (progn
+    (let ((geometry-params
+	    '((internal-border-width . 0)
+	      (fullscreen . maximized)
+	      (fullscreen-restore . maximized)
+	      (undecorated . t))))
     (setcdr default-frame-alist geometry-params)
-    (setcdr initial-frame-alist geometry-params))
+    (setcdr initial-frame-alist geometry-params)))
   :config
   (progn
     ;; Kill `suspend-frame' and start Emacs maximized
     (global-unset-key (kbd "C-z"))
     (global-unset-key (kbd "C-x C-z"))
     (unless (frame-parameter nil 'fullscreen)
-      ;; (set-frame-parameter nil 'fullscreen 'maximized)
-      (toggle-frame-fullscreen)
-      (toggle-frame-fullscreen))))
+      (let ((wm (bound-and-true-p >>=window-manager)))
+	(cond
+	  ((or (eq system-type 'darwin) (equal wm "emacs"))
+	    (toggle-frame-fullscreen))
+	((equal wm "i3")
+	  (toggle-frame-fullscreen)
+	  (toggle-frame-fullscreen))
+	(t
+	  (toggle-frame-maximized)))))))
 
 
 (use-package windmove
