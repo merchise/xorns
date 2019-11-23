1072aa28204598bf4c8947350b491bb04a18f746
Medardo Antonio Rodriguez <med@merchise.org>
2019-11-23 14:23:51 -0500

    Remove extra sketch folder
    - This was created as backlog references.

--- horns/extra/xorns-functional.el
+++
@@ -1,18 +0,0 @@
-;;; xorns-functional.el --- Advanced functional programming stuffs
-
-;; Copyright (c) Merchise Autrement [~º/~]
-
-;; This file is NOT part of GNU Emacs but I'd like it. ;)
-
-
-;;; Code:
-
-(require 'use-package)
-
-
-(use-package proof-general
-  :defer t)
-
-
-(provide 'xorns-functional)
-;;; xorns-functional.el ends here
