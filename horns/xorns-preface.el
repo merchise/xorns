758bc5daac813f98c565f76135235eb1606b4f5e
Medardo Antonio Rodriguez <med@merchise.org>
2019-12-20 14:35:03 -0500

    Change how to initially hide 'mode-line'
    - It is migrated to use a function instead a minor-mode.

--- horns/xorns-preface.el
+++ horns/xorns-preface.el
@@ -24,48 +24,19 @@
 ;;; Code:
 
 (with-eval-after-load 'xorns-preface
-  (hidden-mode-line-mode)
+  (>>-ui/hidden-mode-line)
   (>>=ui/remove-useless-bars)
   (prefer-coding-system 'utf-8)
-  (setq locale-coding-system 'utf-8)
-  )
+  (setq locale-coding-system 'utf-8))
 
 
-
-;; Taken from: http://bzg.fr/emacs-hide-mode-line.html
+(defun >>-ui/hidden-mode-line ()
+  "Hide the mode-line in the initial buffer."
+  ;; Based on: http://bzg.fr/emacs-hide-mode-line.html
+  (when mode-line-format
+    (setq mode-line-format nil)
+    (force-mode-line-update)))
 
-(defvar-local hidden-mode-line-mode nil)
-(defvar-local hide-mode-line nil)
-
-
-(define-minor-mode hidden-mode-line-mode
-  "Minor mode to hide the mode-line in the current buffer."
-  :init-value nil
-  :global t
-  :variable hidden-mode-line-mode
-  :group 'editing-basics
-  (if hidden-mode-line-mode
-      (setq hide-mode-line mode-line-format
-            mode-line-format nil)
-    (setq mode-line-format hide-mode-line
-          hide-mode-line nil))
-  (force-mode-line-update)
-  ;; Apparently force-mode-line-update is not always enough to
-  ;; redisplay the mode-line
-  (redraw-display)
-  (when (and (called-interactively-p 'interactive)
-             hidden-mode-line-mode)
-    (run-with-idle-timer
-     0 nil 'message
-     (concat "Hidden Mode Line Mode enabled.  "
-             "Use M-x hidden-mode-line-mode to make the mode-line appear."))))
-
-
-;; If you want to hide the mode-line in every buffer by default
-;; (add-hook 'after-change-major-mode-hook 'hidden-mode-line-mode)
-
-
-
 
 (defun >>=ui/remove-useless-bars ()
   "Remove tool bar, menu bar, and scroll bars."
