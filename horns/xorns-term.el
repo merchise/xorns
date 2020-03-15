dfd2c16114aced5e94aacc9aedacab3ec39352d3
Medardo Antonio Rodriguez <med@merchise.org>
2020-03-14 21:40:49 -0400

    Include commands for 'term-paste' ('C-y'), and 'raw-kill-line' ('C-k')

--- horns/xorns-term.el
+++ horns/xorns-term.el
@@ -18,12 +18,20 @@
 
 
 (use-package term
+  :preface
+  (defun >>-term/raw-kill-line ()
+    "Kill the rest of the current line in `term-char-mode'."
+    (interactive)
+    (term-send-raw-string "\C-k")
+    (kill-line))
   :bind
   (("C-c t" . ansi-term)
    (:map term-mode-map
      ("C-c C-t" . term-char-mode))
    (:map term-raw-map
-     ("C-c C-t" . term-line-mode)))
+     ("C-c C-t" . term-line-mode)
+     ("C-y" . term-paste)
+     ("C-k" . >>-term/raw-kill-line)))
   :custom
   (term-input-autoexpand t))
 
