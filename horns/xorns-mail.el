f990119359a0d511f3b1ebe513a6a7f5b74ac52d
Medardo Antonio Rodriguez <med@merchise.org>
2020-03-27 18:23:47 -0400

    Integrate 'widgets' into 'mail' module

--- horns/xorns-mail.el
+++ horns/xorns-mail.el
@@ -15,17 +15,55 @@
 
 ;;; Code:
 
-
 ;; Requires, auto-loads and declarations
+
+(require 'cl-lib)
 
 (require 'sendmail)
 (require 'smtpmail)
 (require 'message)
 
-(require 'cl-lib)
+(require 'widget)
+(require 'wid-edit)
 
 (require 'xorns-utils)
-(require 'xorns-widgets)
+
+
+;;; Widgets
+(defun --required (widget)
+  "Validate the WIDGET's value as required."
+  (let ((value (widget-value widget)))
+    (when (or (null value) (equal value ""))
+      (widget-put widget :error "This field is required.")
+      widget)))
+
+
+(define-widget 'xorns-smtp-account-line 'lazy
+  "A custom SMTP address line.
+
+A line has the email address, the login, the server and the type of
+connection.  Other data such as password and port should be placed in the
+~/.authinfo file."
+  :tag "SMTP account"
+  :type '(list
+           (string :tag "Email address"
+             :validate --required)
+           (choice :tag "Login"
+             (const :tag "Use email address" full-email-address)
+             (const :tag "User from email address" user-from-email)
+             (string))
+           (string
+             :tag "Server address"
+             :help-echo
+             "The address of the SMTP server for this account.  If left
+empty the address defaults to \"smtp.ADDRESS-DOMAIN\".")
+           (choice
+             (const :tag "Possibly upgrade to STARTTLS" nil)
+             (const :tag "Always use STARTTLS" starttls)
+             (const :tag "Never use STARTTLS" plain)
+             (const :tag "Use TLS/SSL" ssl))
+           ))
+
 
 
 ;; Local definitions
