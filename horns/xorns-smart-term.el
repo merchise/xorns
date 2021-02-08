004f7c9dee8e6665fa1ffd4480f30a2c0c1a1b69
Medardo Antonio Rodriguez <med.merchise@gmail.com>
2021-02-08 09:59:05 -0500

    Start migrating smart terminal to a separate module

---
+++ horns/xorns-smart-term.el
@@ -0,0 +1,13 @@
+;;; xorns-smart-term.el --- Building-block for smart terminals  -*- lexical-binding: t -*-
+
+;; Copyright (c) Merchise Autrement [~º/~]
+
+;;; Commentary:
+
+;; Enjoy!
+
+
+;;; Code:
+
+(provide 'xorns-smart-term)
+;;; xorns-smart-term.el ends here
