eceffb1161c42263628bc1e3850d08b95b7f7e32
Manuel Vázquez Acosta <manuel@merchise.org>
2021-05-05 17:35:08 +0200

    Fix indentation of xorns-buffers.

--- horns/xorns-buffers.el
+++ horns/xorns-buffers.el
@@ -66,117 +66,109 @@
   :custom
   (ibuffer-saved-filter-groups    ; check `ibuffer-saved-filters'
     '(("Normal"
-	("System"
-	  (or
-	    (name . "[*]scratch[*]")
-	    (name . "[*]Messages[*]")
-	    (name . "[*]Backtrace[*]")))
-	("Terms"
-	  (or
-	    (mode . term-mode)
-	    (mode . vterm-mode)
-	    (mode . eshell-mode)))
-	("Dired"
-	  (or
-	    (mode . dired-omit-mode)
-	    (mode . dired-mode)))
-	("GNUs/Org"    ; TODO: Check this
-	  (or
-	    (name . "*Deft*")
-	    (name . "bbdb")
-	    (name . "[.]newsrc-dribble")
-	    (mode . org-mode)
-	    (mode . org-agenda-mode)
-	    (mode . diary-mode)
-	    (mode . calendar-mode)
-	    (mode . bbdb-mode)
-	    (mode . message-mode)
-	    (mode . mail-mode)
-	    (mode . gnus-group-mode)
-	    (mode . gnus-summary-mode)
-	    (mode . gnus-article-mode)))
-	("Configuration" (mode . conf-unix-mode))
-	("Programming"
-          (or
-	    (derived-mode . prog-mode)
-            (mode . ess-mode)
-            (mode . compilation-mode)))
-	("Text Document"
-          (and
-	    (derived-mode . text-mode)
-            (not (starred-name))))
-	("TeX"
-          (or
-	    (derived-mode . tex-mode)
-            (mode . latex-mode)
-            (mode . context-mode)
-            (mode . ams-tex-mode)
-            (mode . bibtex-mode)))
-	("Web"
-          (or
-	    (derived-mode . sgml-mode)
-            (derived-mode . css-mode)
-            (mode . javascript-mode)
- 	    (mode . mhtml-mode)
- 	    (mode . w3m-mode)
-            (mode . js2-mode)
-	    (mode . coffee-mode)
-            (mode . scss-mode)
-            (derived-mode . haml-mode)
-            (mode . sass-mode)))
-	;; ("Web/XML/HTML/CSS"
-	;;   (or
-	;;     (mode . w3m-mode)
-	;;     (mode . nxml-mode)
-	;;     (mode . html-mode)
-	;;     (mode . css-mode)
-	;;     (mode . less-mode)
-	;;     (mode . sass-mode)))
-	("Version Control"
-	  (or
-	    (name . "^magit")
-	    (mode . git-commit-mode)
-	    (mode . git-commit-major-mode)
-	    (mode . git-rebase-mode)
-	    (mode . magit-mode)
-	    (mode . magit-cherry-mode)
-	    (mode . magit-diff-mode)
-	    (mode . magit-log-mode)
-	    (mode . magit-log-select-mode)
-	    (mode . magit-merge-preview-mode)
-	    (mode . magit-popup-mode)
-	    (mode . magit-process-mode)
-	    (mode . magit-refs-mode)
-	    (mode . magit-reflog-mode)
-	    (mode . magit-revision-mode)
-	    (mode . magit-stash-mode)
-	    (mode . magit-stashes-mode)
-	    (mode . magit-status-mode)
-	    (mode . diff-mode)))
-	("Help/Info/Completions/Customize"
-	  (or
-	    (name . "^[*]Help[*]$")
-	    (name . "^[*]info[*]$")
-	    (name . "^[*]helpful")
-	    (name . "^[*]Customize")
-	    (derived-mode . special-mode)
-	    (mode . Info-mode)
-	    (mode . rfcview-mode)
-	    (mode . Custom-mode)
-	    (mode . completion-list-mode)))
-	("X Window Manager" (mode . exwm-mode))
-	)))
+    ("System"
+      (or
+        (name . "[*]scratch[*]")
+        (name . "[*]Messages[*]")
+        (name . "[*]Backtrace[*]")))
+    ("Terms"
+      (or
+        (mode . term-mode)
+        (mode . vterm-mode)
+        (mode . eshell-mode)))
+    ("Dired"
+      (or
+        (mode . dired-omit-mode)
+        (mode . dired-mode)))
+    ("GNUs/Org"    ; TODO: Check this
+      (or
+        (name . "*Deft*")
+        (name . "bbdb")
+        (name . "[.]newsrc-dribble")
+        (mode . org-mode)
+        (mode . org-agenda-mode)
+        (mode . diary-mode)
+        (mode . calendar-mode)
+        (mode . bbdb-mode)
+        (mode . message-mode)
+        (mode . mail-mode)
+        (mode . gnus-group-mode)
+        (mode . gnus-summary-mode)
+        (mode . gnus-article-mode)))
+    ("Configuration" (mode . conf-unix-mode))
+    ("Programming"
+      (or
+        (derived-mode . prog-mode)
+        (mode . ess-mode)
+        (mode . compilation-mode)))
+    ("Text Document"
+      (and
+        (derived-mode . text-mode)
+        (not (starred-name))))
+    ("TeX"
+      (or
+        (derived-mode . tex-mode)
+        (mode . latex-mode)
+        (mode . context-mode)
+        (mode . ams-tex-mode)
+        (mode . bibtex-mode)))
+    ("Web"
+      (or
+        (derived-mode . sgml-mode)
+        (derived-mode . css-mode)
+        (mode . javascript-mode)
+        (mode . mhtml-mode)
+        (mode . w3m-mode)
+        (mode . js2-mode)
+        (mode . coffee-mode)
+        (mode . scss-mode)
+        (derived-mode . haml-mode)
+        (mode . sass-mode)))
+    ("Version Control"
+      (or
+        (name . "^magit")
+        (mode . git-commit-mode)
+        (mode . git-commit-major-mode)
+        (mode . git-rebase-mode)
+        (mode . magit-mode)
+        (mode . magit-cherry-mode)
+        (mode . magit-diff-mode)
+        (mode . magit-log-mode)
+        (mode . magit-log-select-mode)
+        (mode . magit-merge-preview-mode)
+        (mode . magit-popup-mode)
+        (mode . magit-process-mode)
+        (mode . magit-refs-mode)
+        (mode . magit-reflog-mode)
+        (mode . magit-revision-mode)
+        (mode . magit-stash-mode)
+        (mode . magit-stashes-mode)
+        (mode . magit-status-mode)
+        (mode . diff-mode)))
+    ("Help/Info/Completions/Customize"
+      (or
+        (name . "^[*]Help[*]$")
+        (name . "^[*]info[*]$")
+        (name . "^[*]helpful")
+        (name . "^[*]Customize")
+        (derived-mode . special-mode)
+        (mode . Info-mode)
+        (mode . rfcview-mode)
+        (mode . Custom-mode)
+        (mode . completion-list-mode)))
+    ("X Window Manager" (mode . exwm-mode))
+    )))
   (ibuffer-show-empty-filter-groups nil)
   (ibuffer-default-sorting-mode 'major-mode)
   (ibuffer-formats
     '((mark modified read-only vc-status-mini " "
-	(name 24 24 :left :elide)
-	" "
-	(size-h 9 -1 :right)
-	" "
-	(mode 16 16 :left :elide)
-	" "
-	filename-and-process)))
+    (name 24 24 :left :elide)
+    " "
+    (size-h 9 -1 :right)
+    " "
+    (mode 16 16 :left :elide)
+    " "
+    filename-and-process)))
   :hook
   ((ibuffer-mode .
      (lambda () (ibuffer-switch-to-saved-filter-groups "Normal"))))
@@ -198,3 +190,7 @@
 
 (provide 'xorns-buffers)
 ;;; xorns-buffers.el ends here
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
