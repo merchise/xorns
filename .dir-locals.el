c0e3121197f82fa747d9974cd584536e9f2616d4
Medardo Antonio Rodriguez <med.merchise@gmail.com>
2021-05-07 15:51:07 -0400

    Add some directory-local variables

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;; TODO: Configure this in `xorns' modules
+((prog-mode
+  (indent-tabs-mode . nil))
+ (text-mode
+  (indent-tabs-mode . nil))
+ (git-commit-mode
+  (git-commit-major-mode . git-commit-elisp-text-mode)))
