c828d4aa1f90cd438106e7cb749effd5b10f5429
Medardo Antonio Rodriguez <med.merchise@gmail.com>
2021-06-10 09:54:26 -0400

    Refactor setup tools module

--- horns/xorns-setup.el
+++ horns/xorns-setup.el
@@ -21,10 +21,11 @@
 
 (defvar >>=|setup/level nil
   "Desired setup level for `xorns' components.
-There are three possible values defined in `>>=!setup/levels'.")
+An integer value between 0 and 10.  There are three symbols defining standard
+levels in `>>=!setup/levels'.")
 
 
-(defconst >>=!setup/levels '((basic . 0) (medium . 1) (high . 2))
+(defconst >>=!setup/levels '((basic . 0) (medium . 5) (high . 10))
   "Possible setup levels with its associated integer values.")
 
 
@@ -37,38 +38,34 @@ There are three possible values defined in `>>=!setup/levels'.")
       0)
     (t
       (or
-        (if (null level) 0)
         (cdr (assq level >>=!setup/levels))
         (error ">>= invalid setup level '%s'" level)))))
 
 
 (defsubst >>=setup/level-check (&optional level)
-  "Check if LEVEL is OK to setup a feature."
+  "Check if a given setup LEVEL is OK to configure a feature.
+This is done by comparing the given LEVEL against defined `>>=|setup/level'."
   (>=
     (>>-setup/int-level >>=|setup/level)
     (>>-setup/int-level level)))
 
 
 (defun >>=setup/command-check (command &optional level)
-  "Check if LEVEL is OK to setup a feature that depends on a system COMMAND.
-If COMMAND is nil, just check the LEVEL."
+  "Check a COMMAND to find out if a feature can be configured.
+The COMMAND argument must be a string to be checked against `executable-find',
+In this case, a warning will be issued if the COMMAND is not found.  A boolean
+value can also be used, in which case it is returned without further checking.
+If a setup LEVEL is given, it is verified with the `>>=setup/level-check'
+function."
   (and
     (>>=setup/level-check level)
-    (let ((res (executable-find command)))
-      (if res
-        res
-        ;; else
-        (warn ">>= '%s' command is not installed" command)
-        nil))))
-
-
-(defmacro >>=define-setup-checker (command)
-  "Define a `>>=setup/command-check' for a given COMMAND.
-The checker name will be '>>=setup/<command>-check'"
-  (declare (indent 1) (debug t))
-  `(defun ,(intern (format ">>=setup/%s-check" command)) (&optional level)
-     ,(format ">>=setup/command-check for '%s'" command)
-     (>>=setup/command-check ,command level)))
+    (if (booleanp command)
+      command
+      ;; else
+      (let ((res (executable-find command)))
+        (when (null res)
+          (warn ">>= '%s' command is not installed" command))
+        res))))
 
 
 (provide 'xorns-setup)
