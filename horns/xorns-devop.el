91702eee416908317aba3d3968269a9ccef4ee3b
Manuel Vázquez Acosta <mvaled@pm.me>
2024-03-11 11:46:22 +0100

    Add '>>=|devops/features'.
    Also possible features 'caddy' and 'nginx'.

--- horns/xorns-devop.el
+++ horns/xorns-devop.el
@@ -17,32 +17,52 @@
   (require 'use-package))
 
 
-;;; Docker
+;;; Configuration
 
 (defvar >>=|devops/enable t
   "Enable devops-related features: docker, compose, etc.")
 
+(defvar >>=|devops/features '(docker)
+  "Enable specific features of DevOps.")
 
+
+
+;;; Docker
 (use-package docker
-  :when >>=|devops/enable
+  :when (and >>=|devops/enable (memq 'docker >>=|devops/features)
   :ensure t
   :bind ("C-c d" . docker)) ;; TODO: customisable map.
 
 
 (use-package dockerfile-mode
-  :when >>=|devops/enable
+  :when (and >>=|devops/enable (memq 'docker >>=|devops/features)
   :ensure t
   :init
   (add-to-list 'auto-mode-alist '("\\.docker\\'" . dockerfile-mode)))
 
 
 (use-package docker-compose-mode
-  :when >>=|devops/enable
+  :when (and >>=|devops/enable (memq 'docker >>=|devops/features)
   :ensure t)
 
 (use-package lsp-docker
   :ensure t
-  :when >>=|devops/enable)
+  :when (and >>=|devops/enable (memq 'docker >>=|devops/features))
+
+
+;;; Caddy
+
+(use-package caddyfile-mode
+  :when (and >>=|devops/enable (memq 'caddy >>=|devops/features)
+  :ensure t)
+
+
+
+;;; Nginx
+
+(use-package nginx-mode
+  :when (and >>=|devops/enable (memq 'nginx >>=|devops/features)
+  :ensure t)
 
 
 (provide 'xorns-devop)
